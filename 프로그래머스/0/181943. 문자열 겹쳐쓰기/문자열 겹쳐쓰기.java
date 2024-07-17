14ee6af7f521a7ee5f5baf7ee91336c964f49932
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-07-18 01:43:08 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 15.24 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         String prefix = my_string.substring(0, s);
-        String suffix = my_string.substring(overwrite_string.length()+s, my_string.length());
+        String suffix = my_string.substring(overwrite_string.length() + s);
         
         String answer = prefix + overwrite_string + suffix;
         return answer;
