b97892a4547f32a6556286404bfc8c451eb87da6
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-07-20 06:34:20 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.38 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] arr, int idx) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i < idx) {
+                continue;
+            }
+            
+            if (arr[i] == 1) {
+                return i;
+            }
+        }
+        
+        
+        return -1;
+    }
+}
