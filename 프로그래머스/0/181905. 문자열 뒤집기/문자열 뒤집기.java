96dd4ef5a7ea9dbee78d789e29de4baa51986580
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-07-20 05:25:37 +0900

    [level 0] Title: 문자열 뒤집기, Time: 14.92 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(String my_string, int s, int e) {
+        StringBuilder sb = new StringBuilder(my_string.substring(s, e + 1)).reverse();
+
+        return my_string.substring(0, s) + sb.toString() + my_string.substring(e + 1);
+    }
+}
