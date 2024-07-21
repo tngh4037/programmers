32444d9d3d9a8bd3a4260f00a8ac7952a97e1562
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-07-22 06:15:03 +0900

    [level 1] Title: 예산, Time: 1.05 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int sum = 0;
+        
+        Arrays.sort(d);
+        for (int num: d) {
+            sum += num;
+            if (sum > budget) {
+                break;
+            }
+            
+            answer++;
+        }
+        
+        return answer;
+    }
+}
