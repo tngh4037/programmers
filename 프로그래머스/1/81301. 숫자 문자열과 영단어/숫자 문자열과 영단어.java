43f8af3f56c86cfcf218bc548ed7f1f4286d34f2
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-07-23 07:57:11 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 1.89 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int solution(String s) {
+        String answer = "";
+        
+        String word = "";
+        for (char spel: s.toCharArray()) {
+            int code = (int) spel;
+            if (code >= 48 && code <= 57) { // 0 ~ 9
+                answer += String.valueOf(spel);
+            } else {
+                word += String.valueOf(spel);
+                Integer result = getMatchedNum(word);
+                if (result != null) {
+                    answer += String.valueOf(result);
+                    word = "";
+                }
+            }
+        }
+        
+        return Integer.parseInt(answer);
+    }
+    
+    
+    private Integer getMatchedNum(String word) {
+        switch (word) {
+            case "zero":
+                return 0;
+            case "one":
+                return 1;
+            case "two":
+                return 2;
+            case "three":
+                return 3;
+            case "four":
+                return 4;
+            case "five":
+                return 5;
+            case "six":
+                return 6;
+            case "seven":
+                return 7;
+            case "eight":
+                return 8;
+            case "nine":
+                return 9;
+            default:
+                return null;
+        }
+    }
+}
