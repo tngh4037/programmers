84b230825b64a7e21b99fbbdc51b474bc3ab1a24
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-07-25 09:48:22 +0900

    [level 0] Title: 두 수의 합, Time: 104.35 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,11 +1,71 @@
-import java.math.BigInteger;
+import java.util.*;
 
 class Solution {
     public String solution(String a, String b) {
-        BigInteger a1 = new BigInteger(a);
-        BigInteger b1 = new BigInteger(b);
-        BigInteger result = a1.add(b1);
+        String[] as = a.split("");
+        String[] bs = b.split("");
         
-        return result.toString();
+        int diff_length = 0;
+        if (bs.length > as.length) {
+            diff_length = bs.length - as.length;
+            
+            String[] strs = new String[bs.length];
+            for (int i = 0; i < diff_length; i++) {
+                strs[i] = "0";
+            }
+            
+            for (int i = 0; i < as.length; i++) {
+                strs[i + diff_length] = as[i];
+            }
+            
+            as = strs;
+        } else if (as.length > bs.length) {
+            diff_length = as.length - bs.length;
+            
+            String[] strs = new String[as.length];
+            for (int i = 0; i < diff_length; i++) {
+                strs[i] = "0";
+            }
+            
+            for (int i = 0; i < bs.length; i++) {
+                strs[i + diff_length] = bs[i];
+            }
+            
+            bs = strs;
+        }
+
+        int temp = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = as.length-1; i >= 0; i--) {
+            int sum = Integer.parseInt(as[i]) + Integer.parseInt(bs[i]) + temp;
+            String str = "";
+            if (sum > 9) {
+                temp = 1;
+                str = String.valueOf(sum).substring(1, 2);
+            } else {
+                temp = 0;
+                str = String.valueOf(sum);
+            }
+
+            sb.append(str);
+        }
+
+        if (temp > 0) {
+            sb.append("1");
+        }
+        
+        return sb.reverse().toString();
     }
 }
+
+/*
+        import java.math.BigInteger;
+        
+        BigInteger numA = new BigInteger(a);
+        BigInteger numB = new BigInteger(b);
+        BigInteger sum = numA.add(numB);
+        
+        String answer = sum.toString();
+        
+        return answer;
+*/
