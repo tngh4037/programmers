f35c1fb2dfcd76f5cb3b94817f6b51898ff5d006
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-07-29 08:48:15 +0900

    [level 0] Title: 옹알이 (1), Time: 0.98 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        
+        String[] words = new String[]{"aya", "ye", "woo", "ma"};
+        for (String target: babbling) {
+            String str = "";
+            
+            for (int i = 0; i < 4; i++) {
+                for (String word: words) {
+                    if (target.indexOf(word) == 0) {
+                        target = target.replace(word, "");
+                    }
+                }
+            }
+            
+            if (target.equals("")) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
