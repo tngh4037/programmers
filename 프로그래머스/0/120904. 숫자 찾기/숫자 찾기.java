201a4ffc27e6079fc31e3b7cbf08d1a35e2ed5c6
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-01 15:30:24 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -2,19 +2,8 @@ class Solution {
     public int solution(int num, int k) {
         String num_str = String.valueOf(num);
         String k_str = String.valueOf(k);
-        if (!num_str.contains(k_str)) {
-            return -1;
-        }
         
-        int index = 1;
-        for (String temp: num_str.split("")) {
-            if (temp.equals(k_str)) {
-                return index;
-            }
-            
-            index++;
-        }
-        
-        return index;
+        int answer = num_str.indexOf(k_str);
+        return answer == -1 ? -1 : answer + 1;
     }
 }
