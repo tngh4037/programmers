031c324172704f0b0877ce892f0d63c39eb6bdec
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-01 16:56:35 +0900

    [level 1] Title: 폰켓몬, Time: 16.01 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,12 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
         int takeCount = nums.length / 2;
-        int[] copyNums = Arrays.stream(nums).distinct().toArray();
-        if (copyNums.length >= takeCount) {
+        if (set.size() > takeCount) {
             return takeCount;
         }
 
-        return copyNums.length;
+        return set.size();
     }
 }
