c8c91a958e5f30cbe50769806e2bedba38a85f44
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-01 14:43:57 +0900

    [level 0] Title: 7의 개수, Time: 0.48 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        for (int num: array) {
+            for (String str: String.valueOf(num).split("")) {
+                if (str.equals("7")) {
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
