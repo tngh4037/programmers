ae2cf2f47be4d1777808f2ae817ae57de4d84d0c
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-03 23:46:56 +0900

    [level 0] Title: 가까운 수, Time: 3.17 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        List<Integer> targets = new ArrayList<>();
+        int n1 = n;
+        int n2 = n;
+        while (true) {
+            boolean exist = false;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == n1) {
+                    targets.add(array[i]);
+                    break;
+                }
+            }
+            
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == n2) {
+                    targets.add(array[i]);
+                    break;
+                }
+            }
+            
+            if (targets.size() > 0) {
+                break;
+            }
+            
+            n1++;
+            n2--;
+        }
+
+        int[] answer = targets.stream().mapToInt(Integer::intValue).toArray();
+
+        Arrays.sort(answer);
+
+        return answer[0];
+    }
+}
