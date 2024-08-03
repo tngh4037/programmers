92df8c6cf48252296cb5eae2fe0a879a2a0afec3
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-03 22:37:51 +0900

    [level 0] Title: 약수 구하기, Time: 3.68 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -1,14 +1,8 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> arrs = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) {
-                arrs.add(i);
-            }
-        }
-        
-        return arrs.stream().mapToInt(Integer::intValue).toArray();
+        return IntStream.rangeClosed(1, n).filter(v -> n % v == 0).toArray();
     }
 }
