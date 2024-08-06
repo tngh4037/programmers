7ad6773ace6b81dbdaac2b994e9d67e4640ba361
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-07 01:08:40 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.64 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] lines) {
+        int answer = 0;
+        
+        Set<Target> result = new HashSet<>();
+        for (int[] line: lines) {
+            int start = line[0];  // 0
+            int end = line[1];  // 5
+            
+            for (int i = start; i < end; i++) {
+                Target target = new Target(i, i+1);
+                boolean result_add = result.add(target);
+                if (!result_add) {
+                    result.remove(target);
+                    answer++;
+                }
+            }
+        }
+        
+        
+        return answer;
+    }
+    
+    static class Target {
+        int start;
+        int end;
+
+        public Target(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.start;
+        }
+
+        @Override
+        public boolean equals(Object t) {
+            Target t1 = (Target) t;
+            return t1.start == this.start && t1.end == this.end;
+        }
+    }
+}
