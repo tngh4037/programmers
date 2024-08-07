e379662901502658b1e7abbc7ffc5eb9ebfab0f8
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-08 03:11:38 +0900

    [level 0] Title: 안전지대, Time: 0.04 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -24,49 +24,58 @@ class Solution {
     }
     
     private void drawDangerArea(int[][] board, int row, int col) {
-
+        // 지뢰가 있는 셀 기준으로, 해당 셀을 감싸고 있는 셀 중, 지뢰가 없는 지역에 한해서 위험지역(2) 세팅
+         
+        // 왼쪽 위 셀
         if (row - 1 >= 0 && col - 1 >= 0) {
             if (board[row - 1][col - 1] == 0) {
                 board[row - 1][col - 1] = 2;
             }
         }
         
+        // 위 셀
         if (row - 1 >= 0) {
             if (board[row - 1][col] == 0) {
                 board[row - 1][col] = 2;
             }
         }
         
+        // 오른쪽 위 셀
         if (row - 1 >= 0 && col + 1 < board[0].length) {
             if (board[row - 1][col + 1] == 0) {
                 board[row - 1][col + 1] = 2;
             }
         }
         
+        // 왼쪽 셀
         if (col - 1 >= 0) {
             if (board[row][col - 1] == 0) {
                 board[row][col - 1] = 2;
             }
         }
         
+        // 오른쪽 셀
         if (col + 1 < board[0].length) {
             if (board[row][col + 1] == 0) {
                 board[row][col + 1] = 2;
             }
         }
         
+        // 하단 왼쪽 셀
         if (row + 1 < board.length && col - 1 >= 0) {
             if (board[row + 1][col - 1] == 0) {
                 board[row + 1][col - 1] = 2;
             }
         }
         
+        // 하단 셀
         if (row + 1 < board.length) {
             if (board[row + 1][col] == 0) {
                 board[row + 1][col] = 2;
             }
         }
 
+        // 하단 오른쪽 셀
         if (row + 1 < board.length && col + 1 < board[0].length) {
             if (board[row + 1][col + 1] == 0) {
                 board[row + 1][col + 1] = 2;
