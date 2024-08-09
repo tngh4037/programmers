2d889678d2e8b3607255257b976f1397611451b4
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-09 09:20:27 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 3.68 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,14 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
-        int[] answer;
-        
         String str = my_string.replaceAll("[^0-9]", "");
-        
-        answer = Arrays.stream(str.split("")).mapToInt(Integer::parseInt).toArray();
-        
-        Arrays.sort(answer);
-        
-        return answer;
+
+        return Arrays.stream(str.split("")).sorted().mapToInt(Integer::parseInt).toArray();
     }
 }
