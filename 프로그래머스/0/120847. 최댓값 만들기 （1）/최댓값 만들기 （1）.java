159ed24f7e60a7cea2f9d5d42f89455bcecb79d6
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-10 09:05:57 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.11 ms, Memory: 69 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        
+        int maxValue = numbers[0] * numbers[1];
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i+1; j < numbers.length; j++) {
+                int temp = numbers[i] * numbers[j];
+                if (maxValue < temp) {
+                    maxValue = temp;
+                }
+            }
+        }
+        
+        return maxValue;
+    }
+}
