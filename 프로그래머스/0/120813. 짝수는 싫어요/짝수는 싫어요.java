6d6b92540a418ce43802c85545d226803b8cfa5f
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-16 10:46:41 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.94 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -5,9 +5,7 @@ class Solution {
     public int[] solution(int n) {
         int[] answer = answer = IntStream.rangeClosed(1, n)
             .filter(i -> i % 2 != 0)
-            .sorted()
             .toArray();
-        
         
         return answer;
     }
