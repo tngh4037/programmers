4ff32810ae080f2467aad3e1e1ff18c3749934ba
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-19 22:31:47 +0900

    [level 0] Title: 배열 만들기 2, Time: 156.29 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int l, int r) {
+        int[] answer = {};
+
+        List<Integer> temp = new ArrayList<>();
+        for (int su = l; su <= r; su++) {
+            String str = String.valueOf(su);
+            
+            for (char c: str.toCharArray()) {
+                int code = (int) c;
+                if (code != 48 && code != 53) {
+                    str = null;
+                    continue;
+                }
+            }
+            
+            if (str != null) {
+               temp.add(Integer.parseInt(str)); 
+            }
+        }
+        
+        answer = temp.stream().mapToInt(Integer::valueOf).toArray();
+        
+        Arrays.sort(answer);
+        
+        return answer.length == 0 ? new int[]{-1} : answer;
+    }
+}
