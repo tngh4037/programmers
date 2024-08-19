82c37210c4b9f6cb71c41986519a8e2d42446571
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-08-19 21:57:29 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 1.42 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,9 @@
+import java.util.stream.*;
+
+class Solution {
+    public int solution(int[] array, int n) {
+        return (int) IntStream.of(array)
+            .filter(v -> v == n)
+            .count();
+    }
+}
