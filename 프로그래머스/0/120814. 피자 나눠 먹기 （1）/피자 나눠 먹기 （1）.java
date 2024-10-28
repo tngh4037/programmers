bdb83857f660bcbd229548f09e2225e09158da5b
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-10-28 19:22:15 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -1,17 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        
-        int sum = 0;
-        while (true) {
-            sum += 7;
-            answer++;
-            
-            if (sum >= n) {
-                break;
-            }
-        }
-        
-        return answer;
+
+        return n % 7 == 0 ? (n / 7) : (n / 7) + 1;
     }
 }
