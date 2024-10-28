633a70b95f87686fd58af47cbd6a3770026730a5
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-10-28 18:58:38 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int n, int k) {
-        int freeBeverageCount = n / 10;
+        int serviceCount = n / 10;
         
-        k = k - freeBeverageCount;
-        
-        int totalPrice = (n * 12000) + (k * 2000);
-        
-        return totalPrice;
+        k -= serviceCount;
+
+        return (n * 12000) + (k * 2000);
     }
 }
