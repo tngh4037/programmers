6adbf2914d54101dd8d5aec45424de185814df5a
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-10-29 11:25:51 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.05 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.java
@@ -1,18 +1,6 @@
 class Solution {
     public int solution(int n) {
-        int num = 1;
-        while (true) {
-            long result = num * num;
-            
-            if (result == n) {
-                return 1;
-            }
-            
-            if (result > n) {
-                return 2;
-            }
-            
-            num++;
-        }
+        int sqrt = (int) Math.sqrt(n);
+        return sqrt * sqrt == n ? 1 : 2;
     }
 }
