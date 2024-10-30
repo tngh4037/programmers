eff9ec7038971fb1942078b7945c97eb797252dd
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-10-31 02:36:01 +0900

    [level 0] Title: n의 배수, Time: 0.03 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181937. n의 배수/n의 배수.java
+++ 프로그래머스/0/181937. n의 배수/n의 배수.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int num, int n) {
-        if (num % n == 0) {
-            return 1;
-        }
-        
-        return 0;
+        return num % n == 0 ? 1 : 0;
     }
 }
