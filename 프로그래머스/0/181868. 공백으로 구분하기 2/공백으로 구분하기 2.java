d47f99239413d8dc9582560acab0371ff9213b45
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-01 01:34:16 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 4.39 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -2,9 +2,29 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String my_string) {
-        return Arrays.stream(my_string.split(" "))
-            .map(v -> v.trim())
-            .filter(v -> !v.isEmpty())
-            .toArray(String[]::new);
+        StringBuilder sb = new StringBuilder();
+        List<String> answer = new ArrayList<>();
+        
+        String[] strArr = my_string.split("");
+        for (String str: strArr) {
+            if (str.equals(" ")) {
+                String temp = sb.toString();
+                if (!temp.isEmpty()) {
+                    answer.add(sb.toString());
+                }
+                
+                sb = new StringBuilder();
+                continue;
+            }
+            
+            sb.append(str);
+        }
+ 
+        String temp = sb.toString();
+        if (!temp.isEmpty()) {
+            answer.add(sb.toString());
+        }
+        
+        return answer.stream().toArray(String[]::new);
     }
 }
