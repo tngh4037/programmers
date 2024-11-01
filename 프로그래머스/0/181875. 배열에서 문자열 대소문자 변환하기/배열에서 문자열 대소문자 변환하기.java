5b1dda218148681a69216730af4e423e43b86e4c
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-02 00:32:37 +0900

    [level 0] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.20 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.java
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.java
@@ -1,10 +1,11 @@
 class Solution {
     public String[] solution(String[] strArr) {
         for (int i = 0; i < strArr.length; i++) {
+            String str = strArr[i];
             if (i % 2 == 0) {
-                strArr[i] = strArr[i].toLowerCase();
+                strArr[i] = str.toLowerCase();
             } else {
-                strArr[i] = strArr[i].toUpperCase();
+                strArr[i] = str.toUpperCase();
             }
         }
         
