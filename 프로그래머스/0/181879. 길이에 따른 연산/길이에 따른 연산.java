c0d20e0857d68bc6da0aa30c9e3c8148a86932df
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-02 00:41:36 +0900

    [level 0] Title: 길이에 따른 연산, Time: 1.76 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -1,18 +1,9 @@
+import java.util.stream.*;
+
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0;
         
-        if (num_list.length >= 11) {
-            for (int i = 0; i < num_list.length; i++) {
-                answer += num_list[i];
-            }
-        } else {
-            answer = 1;
-            for (int i = 0; i < num_list.length; i++) {
-                answer *= num_list[i];
-            }
-        }
-        
-        return answer;
+        return num_list.length >= 11 ? 
+            IntStream.of(num_list).sum() : IntStream.of(num_list).reduce(1, (a, b) -> a * b);
     }
 }
