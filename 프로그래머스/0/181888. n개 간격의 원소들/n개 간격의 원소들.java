f9c96062a4bcaa5b911c6236aea33ed3a3015b55
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-03 03:02:35 +0900

    [level 0] Title: n개 간격의 원소들, Time: 5.37 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.java
@@ -1,14 +1,10 @@
-import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        List<Integer> nums = new ArrayList<>();
-        for (int i = 0; i < num_list.length; i++) {
-            if (i == 0 || (i % n == 0)) {
-                nums.add(num_list[i]);
-            }
-        }
-        
-        return nums.stream().mapToInt(Integer::intValue).toArray();
+        return IntStream.range(0, num_list.length)
+            .filter(i -> i % n == 0)
+            .map(i -> num_list[i])
+            .toArray();
     }
 }
