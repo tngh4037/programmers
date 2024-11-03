65287a7bae731460a4b95fe4fcb833482c54e9e6
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-04 07:33:48 +0900

    [level 0] Title: 카운트 다운, Time: 7.95 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,12 +1,13 @@
+import java.util.stream.*;
 import java.util.*;
 
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        List<Integer> nums = new ArrayList<>();
-        for (int i = start_num; i >= end_num; i--) {
-            nums.add(i);
-        }
         
-        return nums.stream().mapToInt(Integer::intValue).toArray();
+        return IntStream.rangeClosed(end_num, start_num)
+                .mapToObj(Integer::valueOf)
+                .sorted(Comparator.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
