046ab746d9aeeaba31487d37f5d696b4e1a53450
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-05 07:38:27 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.08 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,11 +1,5 @@
-import java.util.stream.*;
-
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        
-        return IntStream.range(0, my_string.length())
-            .filter(i -> my_string.substring(i).equals(is_suffix))
-            .findFirst()
-            .orElse(-1) >= 0 ? 1 : 0;
+        return my_string.endsWith(is_suffix) ? 1 : 0;
     }
 }
