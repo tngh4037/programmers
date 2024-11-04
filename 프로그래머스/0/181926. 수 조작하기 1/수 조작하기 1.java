8694eb2efbd9ab03475692e6f65832e3dfba2703
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-05 08:01:05 +0900

    [level 0] Title: 수 조작하기 1, Time: 54.94 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,18 +1,22 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int n, String control) {
-        for (int i = 0; i < control.length(); i++) {
-            char sep = control.charAt(i);
-            if (sep == 'w') {
-                n = n + 1;
-            } else if (sep == 's') {
-                n = n - 1;
-            } else if (sep == 'd') {
-                n = n + 10;
-            } else {
-                n = n - 10;   
-            }
+ 
+        return Arrays.stream(control.split(""))
+            .mapToInt(s -> getValue(s))
+            .sum() + n;
+    }
+    
+    public int getValue(String str) {
+        if (str.equals("w")) {
+            return 1;
+        } else if (str.equals("s")) {
+            return -1;
+        } else if (str.equals("d")) {
+            return 10;
+        } else {
+            return -10;
         }
-        
-        return n;
     }
 }
