1da0ec206790263ffc7ce6dba196cd506fb8abb2
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-05 07:43:39 +0900

    [level 0] Title: 문자열의 뒤의 n글자, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
+++ 프로그래머스/0/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -1,6 +1,5 @@
 class Solution {
     public String solution(String my_string, int n) {
-        int start = my_string.length() - n;
-        return my_string.substring(start);
+        return my_string.substring(my_string.length() - n);
     }
 }
