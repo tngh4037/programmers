d51404311928d946f0fc3adb972a898ef7388e9d
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-06 07:47:12 +0900

    [level 0] Title: 꼬리 문자열, Time: 2.99 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -1,15 +1,12 @@
+import java.util.Arrays;
+import java.util.stream.*;
+import java.util.function.*;
+
 class Solution {
     public String solution(String[] str_list, String ex) {
-        String answer = "";
-        
-        for (String str: str_list) {
-            if (str.contains(ex)) {
-                continue;
-            }
-            
-            answer += str;
-        }
-        
-        return answer;
+
+        return Arrays.stream(str_list)
+            .filter(Predicate.not(str -> str.contains(ex)))
+            .collect(Collectors.joining());
     }
 }
