32bad6c7543550b6a32d723dc8857554678addda
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-09 16:26:26 +0900

    [level 0] Title: 이어 붙인 수, Time: 4.99 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,15 +1,21 @@
-class Solution {
-    public int solution(int[] num_list) {
-        StringBuilder odd = new StringBuilder();
-        StringBuilder even =  new StringBuilder();
-        for (int num: num_list) {
-            if (num % 2 == 0) {
-                even.append(num);
-            } else {
-                odd.append(num);
-            }
-        }
+import java.util.stream.*;
+import java.util.*;
 
-        return Integer.parseInt(even.toString()) + Integer.parseInt(odd.toString());
+class Solution {
+    
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        String str1 = IntStream.range(0, num_list.length)
+            .filter(i -> num_list[i] % 2 != 0)
+            .mapToObj(i -> String.valueOf(num_list[i]))
+            .collect(Collectors.joining());
+        
+        String str2 = IntStream.range(0, num_list.length)
+            .filter(i -> num_list[i] % 2 == 0)
+            .mapToObj(i -> String.valueOf(num_list[i]))
+            .collect(Collectors.joining());
+        
+        return Integer.parseInt(str2) + Integer.parseInt(str1);
     }
 }
