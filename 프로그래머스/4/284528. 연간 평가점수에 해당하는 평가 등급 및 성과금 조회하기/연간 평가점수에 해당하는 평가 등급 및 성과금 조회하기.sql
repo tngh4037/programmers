9adc9889efdd9190c4a0e910c3497fd4fbd2000a
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-29 09:40:09 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,21 +1,23 @@
 -- 코드를 작성해주세요
-SELECT A.EMP_NO, A.EMP_NAME, B.GRADE, (A.SAL * B.BONUS) AS 'BONUS'
-FROM HR_EMPLOYEES A, (
-        SELECT EMP_NO,
-               CASE
-                    WHEN AVG(SCORE) >= 96 THEN 'S'
-                    WHEN AVG(SCORE) >= 90 THEN 'A'
-                    WHEN AVG(SCORE) >= 80 THEN 'B'
-                    ELSE 'C'
-               END AS GRADE, 
-               CASE
-                    WHEN AVG(SCORE) >= 96 THEN 0.2
-                    WHEN AVG(SCORE) >= 90 THEN 0.15
-                    WHEN AVG(SCORE) >= 80 THEN 0.1
-                    ELSE 0
-               END AS BONUS
-        FROM HR_GRADE 
-        GROUP BY EMP_NO
-) B
+-- 사번, 성명, 평가 등급, 성과금을 조회
+WITH SCORE_INFO AS (
+    SELECT EMP_NO, (SUM(SCORE) / 2) AS BASE_SCORE
+    FROM HR_GRADE
+    GROUP BY EMP_NO
+)
+SELECT  A.EMP_NO, A.EMP_NAME, 
+        CASE
+            WHEN B.BASE_SCORE >= 96 THEN 'S'
+            WHEN B.BASE_SCORE >= 90 THEN 'A'
+            WHEN B.BASE_SCORE >= 80 THEN 'B'
+            ELSE 'C'
+        END AS GRADE,
+        CASE
+            WHEN B.BASE_SCORE >= 96 THEN SAL * 0.2
+            WHEN B.BASE_SCORE >= 90 THEN SAL * 0.15
+            WHEN B.BASE_SCORE >= 80 THEN SAL * 0.1
+            ELSE 0
+        END AS BONUS
+FROM HR_EMPLOYEES A, SCORE_INFO B
 WHERE A.EMP_NO = B.EMP_NO
-ORDER BY A.EMP_NO;
+ORDER BY A.EMP_NO
