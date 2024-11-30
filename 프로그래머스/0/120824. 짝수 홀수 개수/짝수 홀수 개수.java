d991280983365ec09af074de260176806a923e67
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-30 10:06:04 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 3.13 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -1,12 +1,15 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[2];
+        int evenCount = (int) Arrays.stream(num_list)
+            .filter(num -> num % 2 == 0)
+            .count();
         
-        answer[0] = (int) Arrays.stream(num_list).filter(n -> n % 2 == 0).count();
-        answer[1] = (int) Arrays.stream(num_list).filter(n -> n % 2 != 0).count();
+        int oddCount = (int) Arrays.stream(num_list)
+            .filter(num -> num % 2 != 0)
+            .count();
         
-        return answer;
+        return new int[]{evenCount, oddCount};
     }
 }
