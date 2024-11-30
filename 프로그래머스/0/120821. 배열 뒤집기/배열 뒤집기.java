63ac5f8c043fde049a495243affe946f2bb18cc2
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-11-30 10:10:08 +0900

    [level 0] Title: 배열 뒤집기, Time: 3.64 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -1,14 +1,10 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length];
-        
-        for (int i = num_list.length; i > 0; i--) {
-            answer[num_list.length - i] = num_list[i - 1];
-        }
-        
-        return answer;
+
+        return IntStream.range(0, num_list.length)
+            .map(i -> num_list[(num_list.length-1) - i])
+            .toArray();
     }
 }
