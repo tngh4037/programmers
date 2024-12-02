215a677e9b65b0374a48373940c683972154d773
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-12-02 15:07:43 +0900

    [level 0] Title: 문자열 섞기, Time: 19.90 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,12 +1,11 @@
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String str1, String str2) {
-        String answer = "";
-        
-        for (int i = 0; i < str1.length(); i++) {
-            answer += str1.charAt(i);
-            answer += str2.charAt(i);
-        }
-        
-        return answer;
+     
+        return IntStream.range(0, str1.length())
+            .mapToObj(i -> "" + str1.charAt(i) + str2.charAt(i))
+            .collect(Collectors.joining());
     }
 }
