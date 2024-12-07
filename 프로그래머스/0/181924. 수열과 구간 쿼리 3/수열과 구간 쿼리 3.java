e433ba561c46e295e5be998e638d7bab08e8a51c
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-12-07 20:19:02 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.07 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,18 +1,15 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int[] answer = Arrays.copyOf(arr, arr.length);
 
         for (int[] query: queries) {
-            int x = query[0];
-            int y = query[1];
+            int target1 = query[0]; 
+            int target2 = query[1]; 
             
-            int temp = answer[x];
-            answer[x] = answer[y];
-            answer[y] = temp;
+            int temp = arr[target1];
+            arr[target1] = arr[target2];
+            arr[target2] = temp;
         }
         
-        return answer;
+        return arr;
     }
 }
