9100ee3e8ca67f61295cc6ecf51b30bc7b6854f8
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-12-09 02:29:55 +0900

    [level 0] Title: 진료 순서 정하기, Time: 4.65 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -1,21 +1,10 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] emergency) {
-        int[] answer = new int[emergency.length];
 
-        for (int i = 0; i < emergency.length; i++) {
-            int rank = 1;
-            
-            for (int j = 0; j < emergency.length; j++) {
-                if (emergency[i] < emergency[j]) {
-                    rank++;
-                }
-            }
-            
-            answer[i] = rank;
-        }
-        
-        return answer;
+        return Arrays.stream(emergency)
+            .map(num -> ((int) Arrays.stream(emergency).filter(i -> i > num).count()) + 1)
+            .toArray();
     }
 }
