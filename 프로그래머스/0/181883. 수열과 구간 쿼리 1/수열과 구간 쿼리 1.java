6fdfc26e83ff4577a746ffcc91aecd9baa6860df
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2024-12-10 05:12:23 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.02 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,11 +1,8 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         for (int[] query: queries) {
-            int s = query[0];
-            int e = query[1];
-            
-            for (int i = s; i <= e; i++) {
-                arr[i] += 1;
+            for (int i = query[0]; i <= query[1]; i++) {
+                arr[i]++;
             }
         }
         
