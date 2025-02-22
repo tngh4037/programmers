001166ca62d917092e80d1db1906dceb7caa30cb
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-02-23 07:37:05 +0900

    [level 0] Title: 세균 증식, Time: 0.05 ms, Memory: 92.7 MB -BaekjoonHub

--- 프로그래머스/0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int n, int t) {
-        for (int i = 0; i < t; i++) {
-            n = n * 2;
-        }
-        
-        return n;
+        return (int) (n * Math.pow(2, t));
     }
 }
