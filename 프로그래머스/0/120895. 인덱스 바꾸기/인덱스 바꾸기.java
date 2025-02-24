5c7cd7f397d07cd1d7ff0775930e2329f54b9023
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-02-25 08:43:20 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,13 +1,10 @@
-import java.util.stream.IntStream;
-import java.util.stream.Collectors;
-
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        String target1 = String.valueOf(my_string.charAt(num1));
-        String target2 = String.valueOf(my_string.charAt(num2));
+        char[] cArr = my_string.toCharArray();
+        
+        cArr[num1] = my_string.charAt(num2);
+        cArr[num2] = my_string.charAt(num1);
 
-        return IntStream.range(0, my_string.length())
-            .mapToObj(i -> i == num1 ? target2 : (i == num2 ? target1 : String.valueOf(my_string.charAt(i))))
-            .collect(Collectors.joining());
+        return String.valueOf(cArr);
     }
 }
