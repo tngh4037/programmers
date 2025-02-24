fb1ed91135c86ce1f08361167fbe6a428bbe772e
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-02-25 08:37:21 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 3.47 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -1,14 +1,13 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String my_string, int num1, int num2) {
-        String[] myStrArr = my_string.split("");
-        
-        String temp = myStrArr[num1];
-        myStrArr[num1] = myStrArr[num2];
-        myStrArr[num2] = temp;
-        
-        return Arrays.stream(myStrArr).collect(Collectors.joining());
+        String target1 = String.valueOf(my_string.charAt(num1));
+        String target2 = String.valueOf(my_string.charAt(num2));
+
+        return IntStream.range(0, my_string.length())
+            .mapToObj(i -> i == num1 ? target2 : (i == num2 ? target1 : String.valueOf(my_string.charAt(i))))
+            .collect(Collectors.joining());
     }
 }
