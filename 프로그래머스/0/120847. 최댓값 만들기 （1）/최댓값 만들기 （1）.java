9c06dcfb4fb4bfaf8196c5d57129e6d18feddac7
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-02-24 09:31:32 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 1.30 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] numbers) {
+        
         Arrays.sort(numbers);
         
         return numbers[numbers.length-1] * numbers[numbers.length-2];
