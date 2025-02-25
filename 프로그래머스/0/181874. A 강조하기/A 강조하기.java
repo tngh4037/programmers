b02ae49bd25953088756e6b8ff16ab4ad26ba732
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-02-25 09:34:21 +0900

    [level 0] Title: A 강조하기, Time: 1.82 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(String myString) {
-        return myString.toLowerCase().replaceAll("a", "A");
+
+        return Arrays.stream(myString.split(""))
+            .map(s -> s.equals("a") || s.equals("A") ? s.toUpperCase() : s.toLowerCase())
+            .collect(Collectors.joining());
     }
 }
