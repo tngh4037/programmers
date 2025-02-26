a7854b2a13f5e30d357741a86d0b5ae40fdb7300
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-02-27 08:54:07 +0900

    [level 0] Title: 할 일 목록, Time: 6.15 ms, Memory: 97.7 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,8 +1,8 @@
-import java.util.stream.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-
+       
         return IntStream.range(0, todo_list.length)
             .filter(i -> !finished[i])
             .mapToObj(i -> todo_list[i])
