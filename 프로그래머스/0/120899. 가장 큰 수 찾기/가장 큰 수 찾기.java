d4bc93d5eb383901c6a307fdea5d3ffca127f42a
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-02-26 10:23:29 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 3.89 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -1,18 +1,12 @@
+import java.util.List;
 import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 class Solution {
     public int[] solution(int[] array) {
-        int[] copyArray = Arrays.copyOfRange(array, 0, array.length);
-        
-        Arrays.sort(array);
-        
-        int maxValue = array[array.length - 1];
-        
-        int[] indexs = IntStream.range(0, copyArray.length)
-            .filter(i -> copyArray[i] == maxValue)
-            .toArray();
-        
-        return new int[]{maxValue, indexs[0]};
+        List<Integer> list = Arrays.stream(array).boxed().collect(Collectors.toList());
+        int max = list.stream().max(Integer::compareTo).orElse(0);
+        int index = list.indexOf(max);
+        return new int[] {max, index};
     }
 }
