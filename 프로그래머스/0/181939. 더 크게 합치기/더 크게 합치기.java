1c622a4d60b3a0a3162ede76700e08ba32d7fdf5
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-02-27 08:28:58 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.07 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -1,8 +1,10 @@
 class Solution {
     public int solution(int a, int b) {
-        int target1 = Integer.parseInt("" + a + b);
-        int target2 = Integer.parseInt("" + b + a);
+        int answer = 0;
+        
+        int target1 = Integer.parseInt(String.valueOf(a).concat(String.valueOf(b)));
+        int target2 = Integer.parseInt(String.valueOf(b).concat(String.valueOf(a)));
 
-        return target1 >= target2 ? target1 : target2;
+        return (int) Math.max(target1, target2);
     }
 }
