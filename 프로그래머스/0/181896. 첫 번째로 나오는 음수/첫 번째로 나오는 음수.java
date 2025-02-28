1a970e0b2108b0db111ecf706f1fab584c59b1df
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-01 08:39:59 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 3.34 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -1,8 +1,8 @@
-import java.util.stream.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] num_list) {
-        
+
         return IntStream.range(0, num_list.length)
             .filter(i -> num_list[i] < 0)
             .findFirst()
