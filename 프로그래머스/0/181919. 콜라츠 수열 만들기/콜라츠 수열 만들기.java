b1b9b38e687c3bc8914483bb7e3477c226f1eb9f
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-02 12:52:05 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 5.98 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -1,21 +1,12 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.stream.Stream;
 
 class Solution {
     public int[] solution(int n) {
-        List<Integer> answer = new ArrayList<>();
-        while (n != 1) {
-            answer.add(n);
-            
-            if (n % 2 == 0) {
-                n = n / 2;    
-            } else {
-                n = 3 * n + 1;
-            }
-        }
         
-        answer.add(1);
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return  Stream.concat(
+                    Stream.iterate(n, i -> i != 1, i -> i % 2 == 0 ? i / 2 : 3 * i + 1),
+                    Stream.of(1))
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
