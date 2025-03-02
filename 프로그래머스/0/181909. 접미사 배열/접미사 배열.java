181a51d5f3b6190b0ce3108019c7ebb1848ce065
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-02 12:15:03 +0900

    [level 0] Title: 접미사 배열, Time: 5.96 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,9 +1,10 @@
-import java.util.stream.*;
+import java.util.stream.IntStream;
 
 class Solution {
     public String[] solution(String my_string) {
+
         return IntStream.range(0, my_string.length())
-            .mapToObj(i -> my_string.substring(i))
+            .mapToObj(i -> my_string.substring(i, my_string.length()))
             .sorted()
             .toArray(String[]::new);
     }
