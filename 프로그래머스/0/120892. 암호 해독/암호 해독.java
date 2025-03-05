205e2a103d4585beaa1ca0580aea002b5382a691
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-05 18:07:54 +0900

    [level 0] Title: 암호 해독, Time: 3.70 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.java
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -3,8 +3,9 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String cipher, int code) {
+
         return IntStream.range(0, cipher.length())
-            .filter(i -> ((i+1) % code) == 0)
+            .filter(i -> (i + 1) % code  == 0)
             .mapToObj(i -> String.valueOf(cipher.charAt(i)))
             .collect(Collectors.joining());
     }
