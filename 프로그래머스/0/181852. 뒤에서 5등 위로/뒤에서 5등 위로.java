b2ba88684c833a48bd8fabf8174e9c8d8779270d
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-07 20:27:14 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 3.61 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,15 +1,11 @@
 import java.util.stream.IntStream;
-import java.util.Collections;
 
 class Solution {
     public int[] solution(int[] num_list) {
         
         return IntStream.of(num_list)
-            .boxed()
-            .sorted(Collections.reverseOrder())
-            .limit(num_list.length - 5)
-            .mapToInt(Integer::intValue)
             .sorted()
+            .skip(5)
             .toArray();
     }
 }
