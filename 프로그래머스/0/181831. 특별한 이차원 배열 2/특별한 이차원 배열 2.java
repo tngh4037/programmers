776706df78ca9d88930b78c2535141585f0bfff8
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-07 20:33:20 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 7.31 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -1,14 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[][] arr) {
-       
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i][j] != arr[j][i]) {
-                    return 0;
-                }
-            }
-        }
         
-        return 1;
+        return IntStream.range(0, arr.length)
+            .anyMatch(i -> !IntStream.range(0, arr.length).allMatch(j -> arr[i][j] == arr[j][i])) ? 0 : 1;
     }
 }
