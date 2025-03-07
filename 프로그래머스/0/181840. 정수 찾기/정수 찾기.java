579e2435f9fd6122d31d5f88161c38740b03fe96
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-07 20:11:55 +0900

    [level 0] Title: 정수 찾기, Time: 2.26 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -2,8 +2,6 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(int[] num_list, int n) {
-        int result = Arrays.stream(num_list).filter(num -> num == n).findFirst().orElse(0);
-
-        return result > 0 ? 1 : 0;
+        return Arrays.stream(num_list).anyMatch(i -> i == n) ? 1 : 0;
     }
 }
