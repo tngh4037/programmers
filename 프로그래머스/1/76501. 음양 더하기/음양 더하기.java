13da9a3a840d53c238b0fe7b21e2275fc5131cd3
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-09 22:11:30 +0900

    [level 1] Title: 음양 더하기, Time: 3.81 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -1,11 +1,10 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
-        int answer = 0;
-        
-        for (int i = 0; i < absolutes.length; i++) {
-            answer = answer + ((signs[i] ? 1 : -1) * absolutes[i]);
-        }
-        
-        return answer;
+ 
+        return IntStream.range(0, absolutes.length)
+            .map(i -> signs[i] ? absolutes[i] : -absolutes[i])
+            .sum();
     }
 }
