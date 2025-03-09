a8f0bf6827c8d54a0a47a8111a0f2f26ec121b08
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-09 21:28:34 +0900

    [level 4] Title: 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
+++ 프로그래머스/4/284528. 연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기/연간 평가점수에 해당하는 평가 등급 및 성과금 조회하기.sql
@@ -1,23 +1,18 @@
 -- 코드를 작성해주세요
--- 사번, 성명, 평가 등급, 성과금을 조회
-WITH SCORE_INFO AS (
-    SELECT EMP_NO, (SUM(SCORE) / 2) AS BASE_SCORE
-    FROM HR_GRADE
-    GROUP BY EMP_NO
-)
-SELECT  A.EMP_NO, A.EMP_NAME, 
+SELECT  A.EMP_NO, A.EMP_NAME,
         CASE
-            WHEN B.BASE_SCORE >= 96 THEN 'S'
-            WHEN B.BASE_SCORE >= 90 THEN 'A'
-            WHEN B.BASE_SCORE >= 80 THEN 'B'
+            WHEN AVG(B.SCORE) >= 96 THEN 'S'
+            WHEN AVG(B.SCORE) >= 90 THEN 'A'
+            WHEN AVG(B.SCORE) >= 80 THEN 'B'
             ELSE 'C'
-        END AS GRADE,
+        END AS 'GRADE',
         CASE
-            WHEN B.BASE_SCORE >= 96 THEN SAL * 0.2
-            WHEN B.BASE_SCORE >= 90 THEN SAL * 0.15
-            WHEN B.BASE_SCORE >= 80 THEN SAL * 0.1
+            WHEN AVG(B.SCORE) >= 96 THEN A.SAL * 0.2
+            WHEN AVG(B.SCORE) >= 90 THEN A.SAL * 0.15
+            WHEN AVG(B.SCORE) >= 80 THEN A.SAL * 0.1
             ELSE 0
-        END AS BONUS
-FROM HR_EMPLOYEES A, SCORE_INFO B
+        END AS 'BONUS'
+FROM HR_EMPLOYEES A, HR_GRADE B
 WHERE A.EMP_NO = B.EMP_NO
+GROUP BY A.EMP_NO
 ORDER BY A.EMP_NO
