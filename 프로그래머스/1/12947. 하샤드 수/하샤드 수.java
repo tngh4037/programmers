5a13986631ecc3e0b87795dd8255cfeab18754e3
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-09 22:08:09 +0900

    [level 1] Title: 하샤드 수, Time: 12.42 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,12 +1,6 @@
-import java.util.Arrays;
-
 class Solution {
     public boolean solution(int x) {
-        String strX = String.valueOf(x);
-        int sum = Arrays.stream(strX.split(""))
-            .mapToInt(Integer::parseInt)
-            .sum();
-
-        return x % sum == 0;
+        int sum = ("" + x).chars().map(i -> i - '0').sum();
+        return (x % sum == 0) ? true : false;
     }
 }
