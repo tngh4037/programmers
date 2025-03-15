0c41b0ff01d9ab22a17ff8376a3a068144295c47
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-16 02:45:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 5.20 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,10 +1,11 @@
-import java.util.stream.*;
+import java.util.stream.IntStream;
+import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int n) {
-
+ 
         return IntStream.rangeClosed(1, n)
-            .mapToObj(i -> i % 2 == 0 ? "박" : "수")
+            .mapToObj(i -> i % 2 != 0 ? "수" : "박")
             .collect(Collectors.joining());
     }
 }
