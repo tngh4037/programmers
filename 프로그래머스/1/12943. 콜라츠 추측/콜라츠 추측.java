93fee50f51032fed3a570553d0dd394d453075d6
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-16 02:37:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,22 +1,23 @@
 class Solution {
     public int solution(int num) {
-        long number = num;
-        if (number == 1) {
-            return 0;
-        }
+        if (num == 1) return 0;
         
-        for (int i = 1; i <= 500; i++) {
-            if (number % 2 == 0) {
-                number = number / 2;
+        int count = 0;
+        long su = num;
+        while (su != 1) {
+            if (su % 2 == 0) {
+                su = su / 2;
             } else {
-                number = (number * 3) + 1;
+                su = ((su * 3) + 1);
             }
             
-            if (number == 1) {
-                return i;
+            count++;
+            
+            if (count == 500) {
+                return -1;
             }
         }
-        
-        return -1;
+
+        return count;
     }
 }
