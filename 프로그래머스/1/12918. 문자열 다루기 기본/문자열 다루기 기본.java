0df54fbd81629a2f20cd7d0e8ceaad0e17ed8663
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-17 04:01:06 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.07 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,14 +1,12 @@
 class Solution {
     public boolean solution(String s) {
-        if (s.length() == 4 || s.length() == 6) {
-            try {
-                Integer.parseInt(s);
-                return true;
-            } catch (NumberFormatException e) {
-                return false;
-            }
-        }
+        if (s.length() != 4 && s.length() != 6) return false;
         
-        return false;
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
