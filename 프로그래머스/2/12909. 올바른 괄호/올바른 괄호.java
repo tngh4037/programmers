519d28f127ee1ed588c94cb791097f174e2f9737
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-17 04:46:19 +0900

    [level 2] Title: 올바른 괄호, Time: 6.39 ms, Memory: 53.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,21 +1,22 @@
 class Solution {
     boolean solution(String s) {
         int openCount = 0;
+        
         for (int i = 0; i < s.length(); i++) {
-            String str = s.substring(i, i+1);
-            if (str.equals("(")) {
+            char str = s.charAt(i);
+            if (str == '(') {
                 openCount++;
             }
             
-            if (str.equals(")")) {
-                if (openCount < 1) {
+            if (str == ')') {
+                if (openCount == 0) {
                     return false;
                 }
                 
                 openCount--;
             }
         }
-
+        
         return openCount == 0;
     }
 }
