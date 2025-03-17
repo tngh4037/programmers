aac98218a4e71f0d15fc403f8f44359223b02b8e
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-18 02:01:08 +0900

    [level 2] Title: 최솟값 만들기, Time: 6.06 ms, Memory: 55.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,19 +1,13 @@
-import java.util.*;
-import java.util.stream.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
-class Solution
-{
-    public int solution(int[] A, int[] B)
-    {
-        int answer = 0;
-
+class Solution {
+    public int solution(int[] A, int[] B) {
         Arrays.sort(A);
         Arrays.sort(B);
         
-        for (int i = 0; i < A.length; i++) {
-            answer = answer + (A[i] * B[B.length - (i + 1)]);
-        }
-
-        return answer;
+        return IntStream.range(0, A.length)
+            .map(i -> A[i] * B[(A.length-1) - i])
+            .sum();
     }
 }
