10447410112e82fdeae323f9b806341cce928437
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-18 00:54:14 +0900

    [level 4] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/4/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,34 +1,64 @@
 -- 코드를 입력하세요
+# SELECT Y.HISTORY_ID, ((Y.DAILY_FEE * Y.DURATION) - X.DISCOUNT_RATE / 100) AS FEE
+# FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN X (
+#     SELECT  B.HISTORY_ID, A.DAILY_FEE, 
+#             (DATEDIFF(B.END_DATE, B.START_DATE) + 1) AS 'DURATION',
+#             CASE
+#                 WHEN (DATEDIFF(B.END_DATE, B.START_DATE) + 1) >= 90 THEN '90일 이상'
+#                 WHEN (DATEDIFF(B.END_DATE, B.START_DATE) + 1) >= 30 THEN '30일 이상'
+#                 WHEN (DATEDIFF(B.END_DATE, B.START_DATE) + 1) >= 7 THEN '7일 이상'
+#                 ELSE ''
+#             END AS 'DURATION_TYPE'
+#     FROM CAR_RENTAL_COMPANY_CAR A, CAR_RENTAL_COMPANY_RENTAL_HISTORY B
+#     WHERE A.CAR_ID = B.CAR_ID
+#     AND A.CAR_TYPE = '트럭') Y
+# WHERE X.DURATION_TYPE = Y.DURATION_TYPE
+# AND X.CAR_TYPE = '트럭'
+# ORDER BY FEE DESC, Y.HISTORY_ID DESC
 
 
-SELECT B.HISTORY_ID,
-       CASE 
-            WHEN DATEDIFF(END_DATE, START_DATE)+1 >= 90 
-                THEN FLOOR((A.DAILY_FEE - (A.DAILY_FEE * (SELECT ROUND(DISCOUNT_RATE / 100, 2) 
-                                                    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                                                    WHERE DURATION_TYPE = '90일 이상'
-                                                    AND CAR_TYPE = '트럭'))) * (DATEDIFF(END_DATE, START_DATE) + 1))
-            WHEN DATEDIFF(END_DATE, START_DATE)+1 >= 30 
-                THEN FLOOR((A.DAILY_FEE - (A.DAILY_FEE * (SELECT ROUND(DISCOUNT_RATE / 100, 2) 
-                                                    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                                                    WHERE DURATION_TYPE = '30일 이상'
-                                                    AND CAR_TYPE = '트럭'))) * (DATEDIFF(END_DATE, START_DATE) + 1))
-            WHEN DATEDIFF(END_DATE, START_DATE)+1 >= 7 
-                THEN FLOOR((A.DAILY_FEE - (A.DAILY_FEE * (SELECT ROUND(DISCOUNT_RATE / 100, 2) 
-                                                    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
-                                                    WHERE DURATION_TYPE = '7일 이상'
-                                                    AND CAR_TYPE = '트럭'))) * (DATEDIFF(END_DATE, START_DATE) + 1))
-            ELSE FLOOR(A.DAILY_FEE * (DATEDIFF(END_DATE, START_DATE)+1))
-       END AS FEE
-FROM CAR_RENTAL_COMPANY_CAR A, CAR_RENTAL_COMPANY_RENTAL_HISTORY B
+WITH TRUCK_PLAN AS (
+    SELECT DURATION_TYPE, DISCOUNT_RATE
+    FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN
+    WHERE CAR_TYPE = '트럭'
+)
+SELECT  B.HISTORY_ID,
+        FLOOR(((A.DAILY_FEE) - ((A.DAILY_FEE) * 
+                 IFNULL((SELECT DISCOUNT_RATE
+                         FROM TRUCK_PLAN 
+                         WHERE DURATION_TYPE = B.DURATION_TYPE), 0) / 100)) * B.DURATION) AS FEE
+FROM CAR_RENTAL_COMPANY_CAR A, (
+    SELECT  HISTORY_ID, CAR_ID,
+            (DATEDIFF(END_DATE, START_DATE) + 1) AS 'DURATION',
+            CASE
+                WHEN (DATEDIFF(END_DATE, START_DATE) + 1) >= 90 THEN '90일 이상'
+                WHEN (DATEDIFF(END_DATE, START_DATE) + 1) >= 30 THEN '30일 이상'
+                WHEN (DATEDIFF(END_DATE, START_DATE) + 1) >= 7 THEN '7일 이상'
+                ELSE ''
+            END AS 'DURATION_TYPE' 
+    FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY ) B
 WHERE A.CAR_ID = B.CAR_ID
 AND A.CAR_TYPE = '트럭'
-ORDER BY 2 DESC, 1 DESC;
-    
+ORDER BY FEE DESC, B.HISTORY_ID DESC;
 
 
+-- SELECT * FROM CAR_RENTAL_COMPANY_CAR WHERE car_id = 20; -- 168000
+-- SELECT * FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY WHERE HISTORY_ID = 630;
+-- SELECT * FROM CAR_RENTAL_COMPANY_DISCOUNT_PLAN ;
 
+# SELECT COUNT(*)
+# FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY 
+# WHERE CAR_ID IN (
+# SELECT CAR_ID FROM CAR_RENTAL_COMPANY_CAR WHERE CAR_TYPE = '트럭'
+# );
 
--- 자동차 종류가 '트럭'인 자동차의 대여 기록에 대해서
--- 대여 기록 별로 대여 금액(컬럼명: FEE)을 구하여
--- 기록 ID와 대여 금액 리스트를 출력하는 SQL문
+# (SELECT  B.HISTORY_ID, 
+#         CASE
+#             WHEN (DATEDIFF(B.END_DATE, B.START_DATE) + 1) >= 90 THEN '90일 이상'
+#             WHEN (DATEDIFF(B.END_DATE, B.START_DATE) + 1) >= 30 THEN '30일 이상'
+#             WHEN (DATEDIFF(B.END_DATE, B.START_DATE) + 1) >= 7 THEN '7일 이상'
+#             ELSE ''
+#         END AS 'DURATION_TYPE'
+# FROM CAR_RENTAL_COMPANY_CAR A, CAR_RENTAL_COMPANY_RENTAL_HISTORY B
+# WHERE A.CAR_ID = B.CAR_ID
+# AND A.CAR_TYPE = '트럭')
