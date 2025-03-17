68af087961d99c197663c266508a7cf03505f0fc
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-18 02:09:27 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.38 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,25 +1,29 @@
-import java.util.Arrays;
-import java.util.stream.*;
-
 class Solution {
     public String solution(String s) {
-        s = s.toLowerCase();
-        
-        String[] strArr = s.split("");
-        
-        for (int i = 0; i < strArr.length; i++) {
-            if (i == 0) {
-                strArr[0] = strArr[0].toUpperCase();
+        StringBuilder sb = new StringBuilder();
+        boolean isFirst = true;
+        char[] chars = s.toCharArray();
+        for (char chr : chars) {
+            if (Character.isDigit(chr) && isFirst) {
+                isFirst = false;
+                sb.append(chr);
+                continue;
+            }
+             
+            if (chr == ' ') {
+                isFirst = true;
+                sb.append(chr);
                 continue;
             }
             
-            char beforeChar = strArr[i-1].charAt(0);
-            if (beforeChar == ' ') {
-                strArr[i] = strArr[i].toUpperCase();
+            if (isFirst) {
+                sb.append(String.valueOf(chr).toUpperCase());
+                isFirst = false;
+            } else {
+                sb.append(String.valueOf(chr).toLowerCase());
             }
         }
-
-        return Arrays.stream(strArr).collect(Collectors.joining());
+        
+        return sb.toString();
     }
-    
 }
