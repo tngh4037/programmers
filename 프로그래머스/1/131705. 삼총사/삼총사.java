f378e6b702fd8f9592a34f7ad56933165857745f
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-19 06:22:38 +0900

    [level 1] Title: 삼총사, Time: 0.04 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -2,17 +2,10 @@ class Solution {
     public int solution(int[] number) {
         int answer = 0;
         
-        int x;
-        int y;
-        int z;
         for (int i = 0; i < number.length; i++) {
-            x = number[i];
-            for (int j = i + 1; j < number.length; j++) {
-                 y = number[j];
-                for (int k = j + 1; k < number.length; k++) {
-                    z = number[k];
-                    
-                    if (x + y + z == 0) {
+            for (int j = i+1; j < number.length; j++) {
+                for (int k = j+1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) {
                         answer++;
                     }
                 }
