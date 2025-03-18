a9370b3bd396f8119941cd0bf4768ce3d6ac3e81
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-19 03:40:00 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.08 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,12 +1,11 @@
 class Solution {
     public int solution(int n) {
-        String answer = "";
-
-        while (n != 0) {
-            answer = answer + (n % 3);
-            n = n / 3;
-        }
-        
-        return Integer.parseInt(answer, 3);
+        String str = Integer.toString(n, 3);
+        StringBuilder sb = new StringBuilder();
+        sb.append(str);
+        sb.reverse();
+ 
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
+
