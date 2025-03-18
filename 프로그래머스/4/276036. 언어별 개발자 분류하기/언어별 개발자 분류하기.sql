56abcdff819c51e800e5dc82cc50768cad063423
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-19 01:14:54 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,20 +1,17 @@
 -- 코드를 작성해주세요
-SELECT X.GRADE, X.ID, X.EMAIL
+SELECT A.GRADE, A.ID, A.EMAIL
 FROM (
-    SELECT 
-        CASE 
-            WHEN 
-                SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE (CATEGORY = 'Front End')) != 0 
-                &&
-                SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE (NAME = 'Python')) != 0
-            THEN 'A'
-            WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE (NAME = 'C#')) != 0 THEN 'B'
-            WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE (CATEGORY = 'Front End')) != 0 THEN 'C'
-            ELSE ''
-        END AS GRADE,
-            ID,
-            EMAIL
-    FROM DEVELOPERS
-) X
-WHERE X.GRADE != ''
-ORDER BY X.GRADE, X.ID;
+    SELECT
+         CASE 
+             WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python') != 0 &&
+                  SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End') != 0 THEN 'A'
+             WHEN SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#') THEN 'B'
+             WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End') THEN 'C' 
+             ELSE 'NONE'
+         END AS 'GRADE',
+         ID,
+         EMAIL
+    FROM DEVELOPERS 
+) A
+WHERE A.GRADE != 'NONE'
+ORDER BY GRADE, ID
