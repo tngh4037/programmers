bbce13ec63c7bdbd95135679ea6bd0a60b0cb0c4
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-03-19 06:08:55 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.44 ms, Memory: 93.8 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -1,23 +1,23 @@
 class Solution {
     public String solution(String s) {
-        String answer = "";
-        
-        int num = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char str = s.charAt(i);
-            if (str == ' ') {
-                answer += String.valueOf(str);
-                num = 0;
-            } else {
-                if (num % 2 == 0) {
-                    answer += String.valueOf(str).toUpperCase();
-                } else {
-                    answer += String.valueOf(str).toLowerCase();
-                }
-                num++;
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        for (String str: s.split("")) {
+            if (str.equals(" ")) {
+                sb.append(" ");
+                index = 0;
+                continue;
             }
+            
+            if (index % 2 == 0) {
+                sb.append(str.toUpperCase());
+            } else {
+                sb.append(str.toLowerCase());
+            }
+            
+            index++;
         }
         
-        return answer;
+        return sb.toString();
     }
 }
