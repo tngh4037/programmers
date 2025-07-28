0917917df7c47551e10e1933f78a507fcb1329d9
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-28 12:59:33 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 3.01 ms, Memory: 89.4 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.java
@@ -3,7 +3,7 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String my_string, int n) {
-
+        
         return Arrays.stream(my_string.split(""))
             .map(s -> s.repeat(n))
             .collect(Collectors.joining());
