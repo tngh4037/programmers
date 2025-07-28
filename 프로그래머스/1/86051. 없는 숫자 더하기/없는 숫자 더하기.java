b20442d0a6c32922176437d9a9aedcdb217852a5
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-28 17:20:56 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 4.89 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,7 +1,9 @@
-import java.util.stream.*;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] numbers) {
-        return 45 - IntStream.of(numbers).sum();
+      
+        return IntStream.rangeClosed(0, 9).sum() - Arrays.stream(numbers).sum();
     }
 }
