a537038e4fe137321f040bd6e61998c77637e304
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-28 13:26:53 +0900

    [level 0] Title: 가위 바위 보, Time: 2.88 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -3,19 +3,22 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String rsp) {
-
+      
         return Arrays.stream(rsp.split(""))
-            .map(s -> getWinRsp(s))
+            .map(this::getResult)
             .collect(Collectors.joining());
     }
     
-    private String getWinRsp(String rsp) {
-        if (rsp.equals("0")) {
-            return "5";
-        } else if (rsp.equals("2")) {
-            return "0";
-        } else {
-            return "2";
+    private String getResult(String rsp) {
+        switch(rsp) {
+            case "0":
+                return "5";
+            case "2":
+                return "0";
+            case "5":
+                return "2";
+            default:
+                throw new IllegalArgumentException("unsupported rsp");
         }
     }
 }
