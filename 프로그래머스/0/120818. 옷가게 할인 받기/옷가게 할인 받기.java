64fd4dca05b3f651950c92119234f204f6f7c421
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-28 13:17:17 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,14 +1,15 @@
 class Solution {
     public int solution(int price) {
-        
-        if (price >= 500_000) {
-            return (int) (price - (price * 0.2));
-        } else if (price >= 300_000) {
-            return (int) (price - (price * 0.1));
-        } else if (price >= 100_000) {
-            return (int) (price - (price * 0.05));
+
+        if (price >= 500000) {
+            return (int) (price * 0.8);
+        } else if (price >= 300000) {
+            return (int) (price * 0.9);
+        } else if (price >= 100000) {
+            return (int) (price * 0.95);
+        } else {
+            return price;
         }
         
-        return price;
     }
 }
