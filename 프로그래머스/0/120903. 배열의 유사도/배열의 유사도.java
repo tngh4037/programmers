d80786dd78b8cef8e9244d8ebf807a4547979421
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-28 12:43:41 +0900

    [level 0] Title: 배열의 유사도, Time: 3.25 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 class Solution {
     public int solution(String[] s1, String[] s2) {
-
+       
         return (int) Arrays.stream(s1)
-            .filter(s -> Arrays.stream(s2).anyMatch(t -> t.equals(s)))
+            .filter(s -> Arrays.stream(s2).anyMatch(str -> str.equals(s)))
             .count();
     }
 }
