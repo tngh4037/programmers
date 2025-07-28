bd0b2388b2e5be92d1e8cffed4f01586a55a60b9
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-28 13:53:56 +0900

    [level 0] Title: 대문자와 소문자, Time: 3.91 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String my_string) {
-
+        
         return my_string.chars()
-            .mapToObj(c -> c - 'a' < 0 ? String.valueOf((char) c).toLowerCase() : String.valueOf((char) c).toUpperCase())
+            .mapToObj(c -> c >= 'a' && c <= 'z' ? 
+                      String.valueOf((char) c).toUpperCase() : 
+                      String.valueOf((char) c).toLowerCase())
             .collect(Collectors.joining());
     }
 }
