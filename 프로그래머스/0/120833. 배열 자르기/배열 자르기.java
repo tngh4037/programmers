0de8e0eb3f6ef7cc1f805bb15d4782eb85cae132
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-28 12:34:19 +0900

    [level 0] Title: 배열 자르기, Time: 0.03 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -2,6 +2,7 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
+     
         return Arrays.copyOfRange(numbers, num1, num2+1);
     }
 }
