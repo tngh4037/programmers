40339c9eac961f9000338f44ad5657da442c8484
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-28 16:51:04 +0900

    [level 1] Title: 자릿수 더하기, Time: 2.26 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,9 +2,7 @@ import java.util.Arrays;
 
 public class Solution {
     public int solution(int n) {
-        String strN = "" + n;
-
-        return Arrays.stream(strN.split(""))
+        return Arrays.stream(String.valueOf(n).split(""))
             .mapToInt(Integer::parseInt)
             .sum();
     }
