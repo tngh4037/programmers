83eaffacc3b3f8f6afcf444e27ae11fa05711cd9
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-28 17:30:24 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 14.52 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -2,9 +2,11 @@ import java.util.stream.IntStream;
 
 class Solution {
     public String solution(String[] seoul) {
-        return "김서방은 " + IntStream.range(0, seoul.length)
+        int index = IntStream.range(0, seoul.length)
             .filter(i -> seoul[i].equals("Kim"))
             .findFirst()
-            .orElse(-1) + "에 있다";
+            .orElseThrow(() -> new IllegalArgumentException("Unsupported Arguments"));
+
+        return "김서방은 " + index + "에 있다";
     }
 }
