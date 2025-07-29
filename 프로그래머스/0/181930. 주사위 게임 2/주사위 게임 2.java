4dfb8ea4a0485c778b36dcadf2430e7eac18f22c
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 13:16:59 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,17 +1,13 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int solution(int a, int b, int c) {
-        int count = (int) IntStream.of(a, b, c)
-            .distinct()
-            .count();
-        
-        if (count == 3) {
-            return  a + b + c;
-        } else if (count == 2) {
-            return (a + b + c) * ((a*a) + (b*b) + (c*c));
+
+        if (a == b && b == c) {
+            return (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+        } else if (a != b && a != c && b != c) {
+            return a + b + c;
         } else {
-            return (a + b + c) * ((a*a) + (b*b) + (c*c)) * ((a*a*a) + (b*b*b) + (c*c*c));
+            return (a + b + c) * (a*a + b*b + c*c);
         }
+        
     }
 }
