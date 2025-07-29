29882cade3df758cb3ecaf83b8d316ce62c00f59
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 12:05:38 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 17.77 ms, Memory: 157 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr) {
-
+        
         return Arrays.stream(arr)
-            .map(i -> (i >= 50 && i % 2 == 0) ? i / 2 : ((i < 50 && i % 2 != 0) ? i * 2 : i))
+            .map(i -> (i >= 50) ? (i % 2 == 0 ? i / 2 : i) : (i % 2 != 0 ? i * 2 : i))
             .toArray();
     }
 }
