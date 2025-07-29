d400ef93958321da77a390cd3e12c2c001351499
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 13:49:27 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 6.37 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr, int[] delete_list) {
- 
+        
         return Arrays.stream(arr)
-            .filter(i -> !Arrays.stream(delete_list).anyMatch(j -> j == i))
+            .filter(num -> !Arrays.stream(delete_list).anyMatch(e -> num == e))
             .toArray();
     }
 }
