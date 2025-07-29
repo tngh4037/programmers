0b77feca2fcf145a2e963c1c179515d90b8ef5b2
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 14:30:37 +0900

    [level 0] Title: x 사이의 개수, Time: 6.97 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.java
@@ -1,24 +1,10 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(String myString) {
-        List<Integer> stores = new ArrayList<>();
-        
-        int count = 0;
-        for (String str : myString.split("")) {
-            
-            if (str.equals("x")) {
-                stores.add(count);    
-                count = 0;
-                continue;
-            }
-            
-            count++;
-        }
-        
-        stores.add(count);
-        
-        return stores.stream().mapToInt(Integer::intValue).toArray();
+       
+        return Arrays.stream(myString.split("x", -1))
+            .mapToInt(String::length)
+            .toArray();
     }
 }
