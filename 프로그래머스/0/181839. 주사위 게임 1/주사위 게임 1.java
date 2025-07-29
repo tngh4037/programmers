c69fec0ba782d5348b4f6e99191c74a526efffed
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 14:23:00 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -1,14 +1,11 @@
 class Solution {
     public int solution(int a, int b) {
-        boolean isEvenForA = (a % 2 == 0);
-        boolean isEvenForB = (b % 2 == 0);
-        
-        if (isEvenForA && isEvenForB) {
-            return Math.abs(a - b);
-        } else if (isEvenForA || isEvenForB) {
-            return 2 * (a + b);
+        if (a % 2 == 0 && b % 2 == 0) {
+            return Math.abs(a-b);
+        } else if (a % 2 != 0 && b % 2 != 0) {
+            return a*a + b*b;
         } else {
-            return (a*a) + (b*b);
+            return 2 * (a + b);
         }
     }
 }
