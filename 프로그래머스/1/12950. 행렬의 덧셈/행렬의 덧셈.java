f79edc5de0c04c9fb748312b97fabc60515077e6
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 18:27:39 +0900

    [level 1] Title: 행렬의 덧셈, Time: 3.99 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.java
@@ -1,13 +1,13 @@
-import java.util.stream.IntStream;
-
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr1[0].length];
         
-        IntStream.range(0, arr1.length)
-            .forEach(i -> IntStream.range(0, arr1[0].length)
-                            .forEach(j -> answer[i][j] = arr1[i][j] + arr2[i][j]));
-
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                answer[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+        
         return answer;
     }
 }
