b0184915095ebbfeacbb7f53dbacc535a47aadda
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 13:22:37 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,14 +1,16 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] answer = new int[num_list.length+1];
+        int[] answer = new int[num_list.length + 1];
+        
+        // System.arraycopy(뭐를, 몇번째부터, 어디에, 어디서부터, 몇개를 복사해서 넣을건지)
         System.arraycopy(num_list, 0, answer, 0, num_list.length);
         
-        int last_num = num_list[num_list.length-1];
-        int last_second_num = num_list[num_list.length-2];
-        
-        answer[answer.length-1] = last_num > last_second_num ? last_num - last_second_num : last_num * 2;
+        if (num_list[num_list.length - 1] > num_list[num_list.length - 2]) {
+            answer[answer.length - 1] = 
+                num_list[num_list.length - 1] - num_list[num_list.length - 2];
+        } else {
+            answer[answer.length - 1] = num_list[num_list.length - 1] * 2;
+        }
 
         return answer;
     }
