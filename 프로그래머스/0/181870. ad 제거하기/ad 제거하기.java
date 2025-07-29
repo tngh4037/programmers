87abbcb4ec261e3095c38cdfa617b28a56816ffc
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 14:24:34 +0900

    [level 0] Title: ad 제거하기, Time: 4.80 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -1,11 +1,10 @@
 import java.util.Arrays;
-import java.util.function.Predicate;
 
 class Solution {
     public String[] solution(String[] strArr) {
-      
+        
         return Arrays.stream(strArr)
-            .filter(Predicate.not(s -> s.contains("ad")))
+            .filter(str -> !str.contains("ad"))
             .toArray(String[]::new);
     }
 }
