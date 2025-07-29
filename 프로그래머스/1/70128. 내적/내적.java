480309b5ebedbe39c590f35b43ce3efc2ce7376d
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 17:35:22 +0900

    [level 1] Title: 내적, Time: 1.93 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -2,8 +2,8 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] a, int[] b) {
+        
         return IntStream.range(0, a.length)
-            .map(i -> a[i] * b[i])
-            .sum();
+            .reduce(0, (acc, i) -> acc + a[i] * b[i]);
     }
 }
