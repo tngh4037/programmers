c553b14d397e64c5cfa516b8389645b61afa03fe
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 13:40:35 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.05 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,12 +1,10 @@
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
 class Solution {
     public int solution(String myString, String pat) {
-        String myString_new = Arrays.stream(myString.split(""))
-            .map(s -> s.equals("A") ? "B" : "A")
-            .collect(Collectors.joining());
         
-        return myString_new.contains(pat) ? 1 : 0;
+        return myString
+            .replace("A", "a")
+            .replace("B", "A")
+            .replace("a", "B")
+            .contains(pat) ? 1 : 0;
     }
 }
