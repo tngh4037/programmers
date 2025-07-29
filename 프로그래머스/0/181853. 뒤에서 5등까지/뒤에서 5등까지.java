28112ad62fe06aa36c66e7d054ab06097ff3a423
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 14:13:14 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 12.14 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,8 +2,10 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        Arrays.sort(num_list);
-        
-        return Arrays.copyOfRange(num_list, 0, 5);
+       
+        return Arrays.stream(num_list)
+            .sorted()
+            .limit(5)
+            .toArray();
     }
 }
