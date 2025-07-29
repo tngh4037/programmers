bdc5ad50f4821a835df1079e27c7c150636104db
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-29 12:41:49 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 3.91 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
@@ -3,9 +3,9 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String solution(String[] my_strings, int[][] parts) {
-
+        
         return IntStream.range(0, parts.length)
-            .mapToObj(i -> my_strings[i].substring(parts[i][0], parts[i][1] + 1))
+            .mapToObj(i -> my_strings[i].substring(parts[i][0], parts[i][1]+1))
             .collect(Collectors.joining());
     }
 }
