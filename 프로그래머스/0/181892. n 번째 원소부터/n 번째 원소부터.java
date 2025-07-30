7cde1f37cab6b3029b0974f9b80646206499033d
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-30 11:17:10 +0900

    [level 0] Title: n 번째 원소부터, Time: 18.29 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -1,8 +1,10 @@
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-
-        return Arrays.copyOfRange(num_list, n-1, num_list.length);
+        
+        return IntStream.range(n-1, num_list.length)
+            .map(i -> num_list[i])
+            .toArray();
     }
 }
