b3114bd4c634cce3ad37cfb94a8a48000c08d430
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-30 11:24:16 +0900

    [level 0] Title: 카운트 업, Time: 7.28 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -2,8 +2,7 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int start_num, int end_num) {
- 
-        return IntStream.rangeClosed(start_num, end_num)
-            .toArray();
+       
+        return IntStream.rangeClosed(start_num, end_num).toArray();
     }
 }
