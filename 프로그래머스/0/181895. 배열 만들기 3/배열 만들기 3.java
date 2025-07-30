f92b80dabf70543473a5cbf6db80611457760636
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-30 11:08:18 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.45 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,18 +1,14 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        List<Integer> answer = new ArrayList<>();
-        for (int[] interval: intervals) {
-            int toIndex = interval[0];
-            int fromIndex = interval[1];
-
-            for (int i = toIndex; i < fromIndex+1; i++) {
-                answer.add(arr[i]);
-            }
-        }
-
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        int[] target1 = Arrays.copyOfRange(arr, intervals[0][0], intervals[0][1]+1);
+        int[] target2 = Arrays.copyOfRange(arr, intervals[1][0], intervals[1][1]+1);
+        int[] answer = new int[target1.length + target2.length];
+        
+        System.arraycopy(target1, 0, answer, 0, target1.length);
+        System.arraycopy(target2, 0, answer, target1.length, target2.length);
+        
+        return answer;
     }
 }
