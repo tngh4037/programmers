53197409d9432b71588de35e612b550923768ef5
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-30 10:49:13 +0900

    [level 0] Title: 가까운 1 찾기, Time: 4.11 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -2,9 +2,9 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] arr, int idx) {
-
-        return IntStream.range(idx, arr.length)
-            .filter(i -> arr[i] == 1)
+        
+        return IntStream.range(0, arr.length)
+            .filter(i -> i >= idx && arr[i] == 1)
             .findFirst()
             .orElse(-1);
     }
