3ea90b31f9b631d013e4f6dc1eb26535388fc936
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-30 10:37:30 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.12 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.java
@@ -1,14 +1,18 @@
 class Solution {
     public int solution(String binomial) {
-        String[] binomials = binomial.split(" ");
-        String operator = binomials[1];
+        String[] opers = binomial.split(" ");
+        int num1 = Integer.parseInt(opers[0]);
+        int num2 = Integer.parseInt(opers[2]);
         
-        if (operator.equals("+")) {
-            return Integer.parseInt(binomials[0]) + Integer.parseInt(binomials[2]);
-        } else if (operator.equals("-")) {
-            return Integer.parseInt(binomials[0]) - Integer.parseInt(binomials[2]);
-        } else {
-            return Integer.parseInt(binomials[0]) * Integer.parseInt(binomials[2]);
+        switch (opers[1]) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            default:
+                throw new IllegalArgumentException("unsupported operation");
         }
     }
 }
