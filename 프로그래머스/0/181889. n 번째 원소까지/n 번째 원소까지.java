6c01641daebb0a64cc38984042aab4d37b24bebb
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-30 11:27:55 +0900

    [level 0] Title: n 번째 원소까지, Time: 3.64 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.java
@@ -1,8 +1,10 @@
-import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] num_list, int n) {
-   
-        return Arrays.stream(num_list).limit(n).toArray();
+        
+        return IntStream.range(0, n)
+            .map(i -> num_list[i])
+            .toArray();
     }
 }
