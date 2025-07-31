8a0914f4fee6a5ee574f36e5d4b9409540b389c0
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-31 14:42:32 +0900

    [level 0] Title: 날짜 비교하기, Time: 1.19 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,9 +1,9 @@
-import java.time.*;
+import java.time.LocalDate;
 
 class Solution {
     public int solution(int[] date1, int[] date2) {
 
-        return LocalDate.of(date1[0], date1[1], date1[2])
-            .isBefore(LocalDate.of(date2[0], date2[1], date2[2])) ? 1 : 0;
+        return LocalDate.of(date1[0], date1[1], date1[2]).isBefore(
+        LocalDate.of(date2[0], date2[1], date2[2])) ? 1 : 0;
     }
 }
