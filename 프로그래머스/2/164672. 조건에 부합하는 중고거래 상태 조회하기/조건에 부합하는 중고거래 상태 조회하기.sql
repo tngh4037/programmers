66580f1ee87b0c68155947271c0f6c3d95066a03
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-31 15:02:31 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,11 +1,10 @@
 -- 코드를 입력하세요
 SELECT  BOARD_ID, WRITER_ID, TITLE, PRICE, 
-        CASE 
+        CASE
             WHEN STATUS = 'SALE' THEN '판매중'
             WHEN STATUS = 'RESERVED' THEN '예약중'
-            WHEN STATUS = 'DONE' THEN '거래완료'
-            ELSE ''
-        END AS STATUS
+            ELSE '거래완료'
+        END AS 'STATUS'
 FROM USED_GOODS_BOARD 
-WHERE (CREATED_DATE >= '2022-10-05' AND CREATED_DATE < '2022-10-06')
+WHERE CREATED_DATE = '2022-10-05'
 ORDER BY BOARD_ID DESC
