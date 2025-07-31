0a9bf6e896d1e93ef25ec3fa2b4926c49608de22
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-31 13:45:31 +0900

    [level 0] Title: 외계행성의 나이, Time: 4.94 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -3,11 +3,10 @@ import java.util.stream.Collectors;
 
 class Solution {
     public String solution(int age) {
-
-        String strAge = String.valueOf(age);
-
-        return Arrays.stream(strAge.split(""))
-            .map(s -> String.valueOf((char)(Integer.parseInt(s) + 97)))
-            .collect(Collectors.joining());   
+        char zero = 'a';
+        
+        return Arrays.stream(String.valueOf(age).split(""))
+            .map(str -> String.valueOf((char)(zero + Integer.parseInt(str))))
+            .collect(Collectors.joining());
     }
 }
