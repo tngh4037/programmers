9a5067c08b8767b148b150a34a48347e9cc5f004
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-31 13:39:35 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -1,8 +1,12 @@
 class Solution {
     public int solution(int n) {
+        int slice = 6;
         int count = 1;
-        
-        while ((6 * count) % n != 0) {
+        while (true) {
+            if ((slice * count) % n == 0) {
+                break;
+            }
+            
             count++;
         }
         
