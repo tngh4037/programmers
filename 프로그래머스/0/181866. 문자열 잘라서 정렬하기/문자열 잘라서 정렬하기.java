b201fddd6893f86e1409fa6999d502570ae0c896
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-07-31 13:59:21 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 34.42 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
+import java.util.function.Predicate;
 
 class Solution {
     public String[] solution(String myString) {
-       
         return Arrays.stream(myString.split("x"))
-            .filter(s -> s.length() > 0)
+            .filter(Predicate.not(String::isBlank))
             .sorted()
             .toArray(String[]::new);
     }
