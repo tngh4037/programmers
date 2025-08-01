90ba26509463f3953591a0cc24644908126f719e
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-08-01 11:20:06 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 3.32 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -2,9 +2,9 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(String myString, String pat) {
-
+        
         return (int) IntStream.rangeClosed(0, myString.length() - pat.length())
-            .filter(i -> myString.substring(i, pat.length() + i).equals(pat))
+            .filter(i -> myString.substring(i, i + pat.length()).equals(pat))
             .count();
     }
 }
