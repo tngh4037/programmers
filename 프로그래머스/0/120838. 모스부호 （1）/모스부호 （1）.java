4d570da24514ac28ce666371dc1a7e25e6153138
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-08-06 11:07:39 +0900

    [level 0] Title: 모스부호 (1), Time: 1.56 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,45 +1,50 @@
-import java.util.*;
+import java.util.Map;
+import java.util.HashMap;
 
 class Solution {
     public String solution(String letter) {
-        String answer = "";
-        
-        String[] letter_arr = letter.split(" ");
-        for (String code: letter_arr) {
-            answer += findAlphabet(code);
-        }
 
-        return answer;
-    }
-    
-    private String findAlphabet(String code) {
-        Map<String, String> codeMap = new HashMap<>();
-        codeMap.put(".-", "a");
-        codeMap.put("-...", "b");
-        codeMap.put("-.-.", "c");
-        codeMap.put("-..", "d");
-        codeMap.put(".", "e");
-        codeMap.put("..-.", "f");
-        codeMap.put("--.", "g");
-        codeMap.put("....", "h");
-        codeMap.put("..", "i");
-        codeMap.put(".---", "j");
-        codeMap.put("-.-", "k");
-        codeMap.put(".-..", "l");
-        codeMap.put("--", "m");
-        codeMap.put("-.", "n");
-        codeMap.put("---", "o");
-        codeMap.put(".--.", "p");
-        codeMap.put("--.-", "q");
-        codeMap.put(".-.", "r");
-        codeMap.put("...", "s");
-        codeMap.put("-", "t");
-        codeMap.put("..-", "u");
-        codeMap.put("...-", "v");
-        codeMap.put(".--", "w");
-        codeMap.put("-..-", "x");
-        codeMap.put("-.--", "y");
-        codeMap.put("--..", "z");
-        return codeMap.get(code);
+        Map<String, String> stores = new HashMap<>();
+        stores.put(".-","a");
+        stores.put("-...","b");
+        stores.put("-.-.","c");
+        stores.put("-..","d");
+        stores.put(".","e");
+        stores.put("..-.","f");
+        stores.put("--.","g");
+        stores.put("....","h");
+        stores.put("..","i");
+        stores.put(".---","j");
+        stores.put("-.-","k");
+        stores.put(".-..","l");
+        stores.put("--","m");
+        stores.put("-.","n");
+        stores.put("---","o");
+        stores.put(".--.","p");
+        stores.put("--.-","q");
+        stores.put(".-.","r");
+        stores.put("...","s");
+        stores.put("-","t");
+        stores.put("..-","u");
+        stores.put("...-","v");
+        stores.put(".--","w");
+        stores.put("-..-","x");
+        stores.put("-.--","y");
+        stores.put("--..","z");
+        
+        StringBuilder answer = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+        for (String chr: letter.split(" ")) {
+            sb.append(chr);
+            
+            String findLetter = stores.get(sb.toString());
+            System.out.println(findLetter);
+            if (findLetter != null) {
+                answer.append(findLetter);
+                sb = new StringBuilder();
+            }
+        }
+        
+        return answer.toString();
     }
 }
