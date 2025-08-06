fe0e008a7c53f0104788679ac22427a268b40351
tngh4037 <154078212+tngh4037@users.noreply.github.com>
2025-08-06 11:25:02 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.03 ms, Memory: 91.3 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.java
@@ -1,15 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public int[][] solution(int[] num_list, int n) {
-        int[][] answer = {};
-
-        int length = num_list.length;
-
-        answer = new int[length/n][n];
-
-        for (int i = 0; i < length; i++) {
-            answer[i/n][i%n] = num_list[i];
+        int[][] answer = new int[num_list.length/n][];
+        
+        for (int i = 0; i < num_list.length; i+=n) {
+            answer[i/n] = Arrays.copyOfRange(num_list, i, i+n);
         }
-
+        
         return answer;
     }
 }
